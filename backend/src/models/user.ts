b4ff87dd0a615aab83b0618bfda0de7c900ab26c
TabindaNoor <tabindanoor415@gmail.com>
2024-04-25 15:29:55 +0500

    created model for user profile

---
+++ backend/src/models/user.ts
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const userSchema= new mongoose.Schema({
+
+    authId:{
+        type: String,
+        required:true
+    },
+
+    email:{
+        type:String,
+        required:true
+    }
+    ,
+    name:{
+        type:String,
+    },
+    addressLine1:{
+        type:String, 
+        
+    }
+    , city:{
+        type:String,       
+    }
+    ,
+    country:{
+        type:String
+    }
+
+})
+
+
+const User = mongoose.model('User',userSchema)
+export default User
