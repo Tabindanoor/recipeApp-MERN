6938e74ed73a73d51d43a55eec01cd4ce6003728
TabindaNoor <tabindanoor415@gmail.com>
2024-04-25 23:56:50 +0500

    user api frontend

--- backend/src/controllers/MyUserController.ts
+++ backend/src/controllers/MyUserController.ts
@@ -1,6 +1,7 @@
 import {Request, Response} from 'express';
+import User from '../models/user';
 
-const createCurrentUser=(req:Request, res:Response)=>{
+const createCurrentUser=async(req:Request, res:Response)=>{
 //    check if user exits
 //  if not create userMyUserRoutes.service
 //  return the user object
@@ -9,8 +10,16 @@ const createCurrentUser=(req:Request, res:Response)=>{
     try {
 
         const {auth0Id} = req.body 
-        
-        
+        const existingUser = await User.findOne({auth0Id})
+        if(existingUser)
+            {
+            return res.send(200).send()
+            }
+
+        const newUser = new User(req.body)
+        await newUser.save()
+        res.status(200).send(newUser.toObject())
+
     } catch (error) {
     console.log(error)
     res.status(res.statusCode).json({message:"Error creating current User"})
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,13 +4,26 @@ import './global.css'
 import { BrowserRouter as Rourter } from 'react-router-dom'
 import AppRoutes from './AppRoutes.tsx'
 import Auth0ProviderWithNavigate from './auth/Auth0ProviderWithNavigate.tsx'
+import {QueryClient, QueryClientProvider} from "react-query"
+
+const queryClient= new QueryClient({
+  defaultOptions: {
+    queries:{
+      refetchOnWindowFocus:false
+    
+    }
+  }
+}
+)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Rourter>
+    <QueryClientProvider client={queryClient}> 
       <Auth0ProviderWithNavigate>
                <AppRoutes/>
-      </Auth0ProviderWithNavigate>
+      </Auth0ProviderWithNavigate>  
+      </QueryClientProvider>
     </Rourter>
   </React.StrictMode>,
 )
