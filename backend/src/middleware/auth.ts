ea001c3a451a3c2e9f77d2d36620da41e88bd484
TabindaNoor <tabindanoor415@gmail.com>
2024-04-28 23:58:01 +0500

    going to controller

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -1,5 +1,17 @@
 import {auth } from "express-oauth2-jwt-bearer"
 import {Request, Response, NextFunction} from "express"
+import jwt  from "jsonwebtoken";
+import User from "../models/user";
+
+
+declare global {
+  namespace Express{
+    interface Request{
+      userId:string;
+      auth0Id:string
+    }
+  }
+}
 export const jwtCheck = auth({
     audience: process.env.AUTH0_AUDIENCE,
     issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
@@ -17,4 +29,23 @@ export const jwtCheck = auth({
     if (!authorization || !authorization.startsWith("Bearer ")) {
         return res.sendStatus(401);
       }
+
+      const token = authorization.split(" ")[1]
+      try {
+        const decoded = jwt.decode(token) as jwt.JwtPayload;
+        const auth0Id = decoded.sub;
+    
+        const user = await User.findOne({ auth0Id });
+    
+        if (!user) {
+          return res.sendStatus(401);
+        }
+    
+        req.auth0Id = auth0Id as string;
+        req.userId = user._id.toString();
+        next();
+      } catch (error) {
+        return res.sendStatus(401);
+      }
   }
+  
