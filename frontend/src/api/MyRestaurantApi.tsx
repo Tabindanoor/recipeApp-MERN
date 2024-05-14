92698b055633f0bd89e02694a34fb9f02f4958ab
TabindaNoor <tabindanoor415@gmail.com>
2024-05-14 15:47:27 +0500

    restaurant api

--- frontend/src/api/MyRestaurantApi.tsx
+++ frontend/src/api/MyRestaurantApi.tsx
@@ -1,4 +1,42 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import { useMutation } from "react-query";
+import { toast } from "sonner";
 
-// const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
+
+export const useCreateMyRestaurant=()=>{
+    const {getAccessTokenSilently} = useAuth0();
+
+    const createMyRestaurantRequest=async(resturantFormData:FormData)=>{
+        const accessToken = await getAccessTokenSilently()
+        const response = await fetch(`${API_BASE_URL}/api/my/restaurant`,{
+            method:"POST",
+            headers:{
+                Authorization: `Bearer ${accessToken}`,
+
+            },
+            body:resturantFormData,
+        });
+
+        if(!response.ok){
+            throw new Error("Could not create restaurant");
+        }
+        return response.json()
+    };
+
+    const {
+        mutate:createRestaurant ,
+            isLoading,
+            isSuccess,
+            error
+     }= useMutation(createMyRestaurantRequest);
+
+     if(isSuccess){
+        toast.success("Restaurant created");
+     }
+     if(error){
+        toast.error("Unable to update restaurant")
+     }
+     return { createRestaurant, isLoading};
+};
