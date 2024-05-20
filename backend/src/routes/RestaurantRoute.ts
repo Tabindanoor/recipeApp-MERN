a702df1db01938ea33d3338b2b29fb308d06f0e1
TabindaNoor <tabindanoor415@gmail.com>
2024-05-20 21:32:33 +0500

    search bar frontend

--- backend/src/routes/RestaurantRoute.ts
+++ backend/src/routes/RestaurantRoute.ts
@@ -1,18 +1,48 @@
+// import express from "express";
+// import { param } from "express-validator";
+// import MyRestaurantController from "../controllers/MyRestaurantController";
+// import RestaurantController from "../controllers/RestaurantController";
+
+// const router= express.Router();
+
+// router.get(
+//     "/search/:city",
+//     param("city")
+//     .isString()
+//     .trim()
+//     .notEmpty()
+//     .withMessage("City parameter must be a valid string"),
+//     RestaurantController.searchRestaurant
+// );
+
+// export default router;
+
+
+
 import express from "express";
 import { param } from "express-validator";
-import MyRestaurantController from "../controllers/MyRestaurantController";
 import RestaurantController from "../controllers/RestaurantController";
 
-const router= express.Router();
+const router = express.Router();
+
+// router.get(
+//   "/:restaurantId",
+//   param("restaurantId")
+//     .isString()
+//     .trim()
+//     .notEmpty()
+//     .withMessage("RestaurantId paramenter must be a valid string"),
+//   RestaurantController.getRestaurant
+// );
 
 router.get(
-    "/search/:city",
-    param("city")
+  "/search/:city",
+  param("city")
     .isString()
     .trim()
     .notEmpty()
-    .withMessage("City parameter must be a valid string"),
-    RestaurantController.searchRestaurant
+    .withMessage("City paramenter must be a valid string"),
+  RestaurantController.searchRestaurant
 );
 
 export default router;
