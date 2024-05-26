6cd28c998294a93a2c9acc050553c057d7590f67
TabindaNoor <tabindanoor415@gmail.com>
2024-05-27 00:57:45 +0500

    menu item

--- backend/src/routes/RestaurantRoute.ts
+++ backend/src/routes/RestaurantRoute.ts
@@ -1,39 +1,18 @@
-// import express from "express";
-// import { param } from "express-validator";
-// import MyRestaurantController from "../controllers/MyRestaurantController";
-// import RestaurantController from "../controllers/RestaurantController";
-
-// const router= express.Router();
-
-// router.get(
-//     "/search/:city",
-//     param("city")
-//     .isString()
-//     .trim()
-//     .notEmpty()
-//     .withMessage("City parameter must be a valid string"),
-//     RestaurantController.searchRestaurant
-// );
-
-// export default router;
-
-
-
 import express from "express";
 import { param } from "express-validator";
 import RestaurantController from "../controllers/RestaurantController";
 
 const router = express.Router();
 
-// router.get(
-//   "/:restaurantId",
-//   param("restaurantId")
-//     .isString()
-//     .trim()
-//     .notEmpty()
-//     .withMessage("RestaurantId paramenter must be a valid string"),
-//   RestaurantController.getRestaurant
-// );
+router.get(
+  "/:restaurantId",
+  param("restaurantId")
+    .isString()
+    .trim()
+    .notEmpty()
+    .withMessage("RestaurantId paramenter must be a valid string"),
+  RestaurantController.getRestaurant
+);
 
 router.get(
   "/search/:city",
