e975fae2821a11726ac7a37ea337f9d1f0995f2a
TabindaNoor <tabindanoor415@gmail.com>
2024-06-01 15:37:38 +0500

    not done with project

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -1,8 +1,11 @@
 import hero from '../assets/hero.png'
+import { SwipeCarousel } from './SwipeCarousel'
+
 const Hero = () => {
   return (
     <div >
         <img src={hero} alt="" className='object-cover w-full min-h[600px]' />
+        <SwipeCarousel/>
     </div>
   )
 }
